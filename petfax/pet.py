9bc798913260b7f1ac884944cb8d3e9697462402
Jeffery Grigsby <jeffery.grigsby@yahoo.com>
2024-06-03 17:47:38 -0700

    assignment done

---
+++ petfax/pet.py
@@ -0,0 +1,16 @@
+from flask import ( Blueprint, render_template )
+import json
+
+pets = json.load(open('pets.json'))
+print (pets)
+
+bp = Blueprint ('pet', __name__, url_prefix="/pets")
+
+
+@bp.route ('/')
+def index():
+    return render_template('index.html', pets=pets)
+
+@bp.route('/<int:pet_id>')
+def pet(pet_id):
+    return render_template('pet.html', pet=pets[pet_id-1])
